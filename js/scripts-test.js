25faa48ec0e93cc986dd66a4913a17b9e02d7489
Connor Hansen <chansen13@georgefox.edu>
2021-08-09 13:15:35 -0700

    add cipher to project list.

--- js/scripts-test.js
+++ js/scripts-test.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function() {
+
 
 function add(number1, number2) {
   return number1 + number2;
@@ -16,9 +16,15 @@ function divide(number1, number2) {
   return number1 / number2;
 }
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = multiply(number1, number2);
+function toCelsius(farenheit) {
+  var celsius = parseInt((farenheit - 32) * 5/9);
+  return celsius;
+}
+
+const farenheit = parseInt(prompt("Enter a number:"));
+//const number1 = parseInt(prompt("Enter a number:"));
+// const number2 = parseInt(prompt("Enter another number:"));
+const result = toCelsius(farenheit);
+// const result = multiply(number1, number2);
 alert(result);
 
-)};
